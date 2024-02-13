d3d510486972d86b315bae1a025d98f72c11d9fe
andres-ordonezs <andresmw.89@gmail.com>
2024-02-12 19:53:23 -0500

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Script succesfully loaded");
+
+$("form").on("submit", function (evt) {
+
+  console.log('form submitted');
+  console.log(evt);
+  evt.preventDefault();
+
+  $newParagraph = (`<p>The movie is called ${$("#movie-title").val()} and its rating is ${$('#movie-rating').val()}</p>`);
+
+  $('.movie-container').append($newParagraph);
+});
